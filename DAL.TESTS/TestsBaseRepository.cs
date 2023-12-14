03e7af1743c7b0806d6d8d6dee434925369bea33
MikNick <kryliev.mykyta@lll.kpi.ua>
2023-12-14 20:21:30 +0200

    Created test for Delete()

--- DAL.TESTS/TestsBaseRepository.cs
+++ DAL.TESTS/TestsBaseRepository.cs
@@ -76,5 +76,44 @@ namespace DAL.TESTS
             //перевіряємо на рівність отриманий і заданий в Setup(mock => mock.Find(2)) об'єкти
             Assert.Equal(expectedFurniture, actionResult);
         }
+
+        [Fact]
+        public void Delete_InputId_CallFindMethodOfFurnitureDbSetThanCallRemoveMethodOfFurnitureDbSet()
+        {
+            //Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<PCSContext>().Options;
+            var mockContext = new Mock<PCSContext>(opt);
+            var mockFurnitureDbSet = new Mock<DbSet<Furniture>>();
+
+            //налаштовуємо проперті Set<Furniture>, dbCotnext'у так,
+            //щоб при виклику він повертав об'єкт моку mockFurnitureDbSet
+            mockContext
+               .Setup(context => context.Set<Furniture>()).Returns(mockFurnitureDbSet.Object);
+
+            Furniture expectedFurniture = new Furniture()
+            {
+                Id = 2,
+                Name = "Стіл дубовий",
+                category = FurnitureCategory.Table
+            };
+
+            //налаштовуємо метод Find, dbSet'у так,
+            //щоб при виклику Find(2) він повертав об'єкт expectedFurniture
+            mockFurnitureDbSet
+                .Setup(mock => mock.Find(2)).Returns(expectedFurniture);
+
+            TestFurnitureRepository repository = new TestFurnitureRepository(mockContext.Object);
+
+            //Act
+            repository.Delete(2);
+
+            //Assert
+            //перевіряємо, що метод Find, dbSet'у був викликаний з параметром (2) один раз
+            mockFurnitureDbSet.Verify(
+                repo => repo.Find(2), Times.Once());
+            //перевіряємо, що метод Remove, dbSet'у був викликаний з параметром expectedFurniture один раз
+            mockFurnitureDbSet.Verify(
+                dbSet => dbSet.Remove(expectedFurniture), Times.Once());
+        }
     }
 }
